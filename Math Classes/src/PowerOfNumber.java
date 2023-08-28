093d609e2ccd2f23457a858545bf9481ab369e11
kirankumar <138993260+kiran-181131@users.noreply.github.com>
2023-08-28 17:33:37 +0530

    Add files via upload
    java

---
+++ Math Classes/src/PowerOfNumber.java
@@ -0,0 +1,8 @@
+public class PowerOfNumber {
+    public static void main(String[] args) {
+        double base = 2;
+        double exponent = 3;
+        double result = Math.pow(base, exponent);
+        System.out.println(base + " raised to the power " + exponent + " is: " + result);
+    }
+}
---
+++ Math Classes/src/SquareRoot.java
@@ -0,0 +1,7 @@
+public class SquareRoot {
+    public static void main(String[] args) {
+        double number = 25;
+        double squareRoot = Math.sqrt(number);
+        System.out.println("Square root of " + number + " is: " + squareRoot);
+    }
+}
